fbac3b0bf1fd96db3018f04049817af284cdb581
Siddharth <dsiddharth2@gmail.com>
2019-09-25 00:26:13 +0530

    Creating the store completed, WIP : Saga react

--- client-side-react-code/src/index.js
+++ client-side-react-code/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App/App';
+import App from './containers/App';
+import { Provider } from "react-redux";
+import store from "./store";
 import * as serviceWorker from './serviceWorker';
 
 const MOUNT_NODE = document.getElementById('root');
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     MOUNT_NODE
 );
 
