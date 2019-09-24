6fc0c3d19a9b89912f051cc214dbeb5ebb85bdcc
Siddharth <dsiddharth2@gmail.com>
2019-09-24 23:33:38 +0530

    Completed the design

--- client-side-react-code/src/containers/App/App.js
+++ client-side-react-code/src/containers/App/App.js
@@ -12,50 +12,51 @@ class App extends React.Component {
 					<div className="content-wrapper">
 						<section className="content-header">
 							<h1>
-								Simple Tables
-								<small>preview of simple tables</small>
+								Article Listing
+								<small>Listing of all the avaliable articles</small>
 							</h1>
-							<ol className="breadcrumb">
-								<li><a href="#"><i className="fa fa-dashboard"></i> Home</a></li>
-								<li><a href="#">Tables</a></li>
-								<li className="active">Simple</li>
-							</ol>
 						</section>
 						<section className="content">
 							<div className="row">
 								<div className="col-md-6">
 									<div className="box">
-										<div className="box-header with-border">
-											<h3 className="box-title">Bordered Table</h3>
-										</div>
 										<div className="box-body">
 											<table className="table table-bordered">
-												<tr>
-													<th>#</th>
-													<th>Task</th>
-													<th>Progress</th>
-													<th>Label</th>
-												</tr>
-												<tr>
-													<td>1.</td>
-													<td>Update software</td>
-													<td>
-														<div className="progress progress-xs">
-															<div className="progress-bar progress-bar-danger"></div>
-														</div>
-													</td>
-													<td><span className="badge bg-red">55%</span></td>
-												</tr>
+												<tbody>
+													<tr>
+														<th>#</th>
+														<th>Article Title</th>
+														<th>Article Description</th>
+													</tr>
+													<tr>
+														<td>1.</td>
+														<td>Update software</td>
+														<td>
+															<p>
+																Some Description
+															</p>
+														</td>
+													</tr>
+													<tr>
+														<td>1.</td>
+														<td>Update software</td>
+														<td>
+															<p>
+																Some Description
+															</p>
+														</td>
+													</tr>
+													<tr>
+														<td>1.</td>
+														<td>Update software</td>
+														<td>
+															<p>
+																Some Description
+															</p>
+														</td>
+													</tr>
+												</tbody>
 											</table>
-										</div>
-										<div className="box-footer clearfix">
-											<ul className="pagination pagination-sm no-margin pull-right">
-												<li><a href="#">&laquo;</a></li>
-												<li><a href="#">1</a></li>
-												<li><a href="#">2</a></li>
-												<li><a href="#">3</a></li>
-												<li><a href="#">&raquo;</a></li>
-											</ul>
 										</div>
 									</div>
 								</div>
